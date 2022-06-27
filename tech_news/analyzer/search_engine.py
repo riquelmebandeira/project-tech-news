e7bbf73890b905bee161e713b68732c2bb2c8e57
riquelme <riquelmebandeira1@outlook.com>
2022-06-27 12:44:12 -0300

    feat: search_by_category function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -32,4 +32,8 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = list(
+        search_news({"category": {"$regex": category, "$options": "i"}})
+    )
+
+    return get_news_title_and_url(news)
